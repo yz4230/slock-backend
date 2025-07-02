7b3f181270a4d941c68c045868081aebf316c77d
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2025-07-02 13:56:57 +0900

    dev: update OpenAPI schema

--- src/main/kotlin/dev/ishiyama/slock/generated/Schemas.kt
+++ src/main/kotlin/dev/ishiyama/slock/generated/Schemas.kt
@@ -12,7 +12,7 @@ public object Schemas {
   public data class Channel(
     public val id: String,
     public val name: String,
-    public val description: String?,
+    public val description: String,
     public val isDirect: Boolean,
     public val createdAt: String,
     public val updatedAt: String,
@@ -21,69 +21,31 @@ public object Schemas {
   @Serializable
   public data class CreateChannel(
     public val name: String,
-    public val description: String?,
+    public val description: String,
     public val isDirect: Boolean,
   )
 
   @Serializable
   public data class CreateChannelRequest(
-    public val channel: Channel,
-  ) {
-    @Serializable
-    public data class Channel(
-      public val name: String,
-      public val description: String?,
-      public val isDirect: Boolean,
-    )
-  }
+    public val channel: Schemas.CreateChannel,
+  )
 
   @Serializable
   public data class CreateChannelResponse(
-    public val channel: Channel,
-  ) {
-    @Serializable
-    public data class Channel(
-      public val id: String,
-      public val name: String,
-      public val description: String?,
-      public val isDirect: Boolean,
-      public val createdAt: String,
-      public val updatedAt: String,
-    )
-  }
+    public val channel: Schemas.Channel,
+  )
 
   @Serializable
   public data class ListChannelsResponse(
-    public val items: List<Items>,
-  ) {
-    @Serializable
-    public data class Items(
-      public val id: String,
-      public val name: String,
-      public val description: String?,
-      public val isDirect: Boolean,
-      public val createdAt: String,
-      public val updatedAt: String,
-    )
-  }
+    public val items: List<Schemas.Channel>,
+  )
 
   @Serializable
   public data class ListMessagesResponse(
-    public val items: List<Items>,
+    public val items: List<Schemas.Message>,
     public val topCursor: String?,
     public val bottomCursor: String?,
-  ) {
-    @Serializable
-    public data class Items(
-      public val id: String,
-      public val userId: String,
-      public val channelId: String,
-      public val threadId: String?,
-      public val content: String,
-      public val createdAt: String,
-      public val updatedAt: String,
-    )
-  }
+  )
 
   @Serializable
   public data class LoginRequest(
@@ -93,18 +55,9 @@ public object Schemas {
 
   @Serializable
   public data class LoginResponse(
-    public val user: User,
+    public val user: Schemas.User,
     public val token: String,
-  ) {
-    @Serializable
-    public data class User(
-      public val id: String,
-      public val name: String,
-      public val email: String,
-      public val createdAt: String,
-      public val updatedAt: String,
-    )
-  }
+  )
 
   @Serializable
   public data class Message(
@@ -126,18 +79,9 @@ public object Schemas {
 
   @Serializable
   public data class RegisterResponse(
-    public val user: User,
+    public val user: Schemas.User,
     public val token: String,
-  ) {
-    @Serializable
-    public data class User(
-      public val id: String,
-      public val name: String,
-      public val email: String,
-      public val createdAt: String,
-      public val updatedAt: String,
-    )
-  }
+  )
 
   @Serializable
   public data class User(
