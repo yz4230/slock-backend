100e0da058318c5cad0d60aa914ccfa425c55d28
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2025-07-02 14:19:45 +0900

    refactor: enhance transaction management with context interface

--- src/main/kotlin/dev/ishiyama/slock/core/repository/TransactionManager.kt
+++ src/main/kotlin/dev/ishiyama/slock/core/repository/TransactionManager.kt
@@ -1,5 +1,11 @@
 package dev.ishiyama.slock.core.repository
 
+interface TransactionContext {
+    fun commit()
+
+    fun rollback()
+}
+
 interface TransactionManager {
-    fun <T> start(block: () -> T): T
+    fun <T> start(block: TransactionContext.() -> T): T
 }
--- src/main/kotlin/dev/ishiyama/slock/core/repository/TransactionManagerImpl.kt
+++ src/main/kotlin/dev/ishiyama/slock/core/repository/TransactionManagerImpl.kt
@@ -1,7 +1,16 @@
 package dev.ishiyama.slock.core.repository
 
+import org.jetbrains.exposed.v1.jdbc.JdbcTransaction
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 
+class TransactionContextImpl(
+    val jdbcTransaction: JdbcTransaction,
+) : TransactionContext {
+    override fun commit() = jdbcTransaction.commit()
+
+    override fun rollback() = jdbcTransaction.rollback()
+}
+
 class TransactionManagerImpl : TransactionManager {
-    override fun <T> start(block: () -> T): T = transaction { block() }
+    override fun <T> start(block: TransactionContext.() -> T): T = transaction { block(TransactionContextImpl(this)) }
 }
