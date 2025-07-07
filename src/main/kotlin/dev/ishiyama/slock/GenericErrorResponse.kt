944f6dde2995f45d6cb7f2680869c00c31fc26f5
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2025-07-07 17:05:27 +0900

    chore: remove unused class

--- src/main/kotlin/dev/ishiyama/slock/GenericErrorResponse.kt
+++
@@ -1,9 +0,0 @@
-package dev.ishiyama.slock
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-class GenericErrorResponse(
-    override val code: Int,
-    override val message: String,
-) : ErrorResponse
